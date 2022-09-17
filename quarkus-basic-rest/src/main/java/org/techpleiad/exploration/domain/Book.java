66ccd0022e4d410d54d0e788ce3d2b070c5fd46e
dhruv-bansal <dhruv.bansal@outlook.com>
2022-09-17 22:46:46 +0530

    microservices structure

---
+++ quarkus-basic-rest/src/main/java/org/techpleiad/exploration/domain/Book.java
@@ -0,0 +1,13 @@
+package org.techpleiad.exploration.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Book {
+    public int id;
+    public String title;
+    public String author;
+    public int yearOfPublication;
+    public String genre;
+
+}
