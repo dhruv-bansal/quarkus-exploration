69b1fb2bfc705af01420dd17d190562afd70caab
dhruv-bansal <dhruv.bansal@outlook.com>
2022-09-17 12:58:32 +0530

    book resource and test cases

---
+++ src/test/java/org/techpleiad/exploration/web/BookResourceNativeTests.java
@@ -0,0 +1,8 @@
+package org.techpleiad.exploration.web;
+
+import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@NativeImageTest
+public class BookResourceNativeTests extends BookResourceITTest{
+}
